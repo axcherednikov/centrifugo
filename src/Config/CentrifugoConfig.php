5fb4665a385f207b33ca139883b1e236e517d1bf
Alexander Cherednikov <excent63@gmail.com>
2024-11-22 00:37:34 +0300

    Expanded the config for the Centrifugo

--- src/Config/CentrifugoConfig.php
+++ src/Config/CentrifugoConfig.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
-final readonly class CentrifugoConfig
+final class CentrifugoConfig
 {
     public function __construct(
-        private string $url,
-        private string $apiKey,
-        private string $secret,
+        private readonly string $url,
+        private readonly string $apiKey,
+        private readonly string $secret,
+        private ?string $certificatePath = null,
+        private ?string $caPath = null,
     ) {
     }
 
@@ -24,5 +26,29 @@ final readonly class CentrifugoConfig
     public function getSecret(): string
     {
         return $this->secret;
+    }
+
+    public function getCertificatePath(): ?string
+    {
+        return $this->certificatePath;
+    }
+
+    public function setCertificatePath(?string $certificatePath): self
+    {
+        $this->certificatePath = $certificatePath;
+
+        return $this;
+    }
+
+    public function getCaPath(): ?string
+    {
+        return $this->caPath;
+    }
+
+    public function setCaPath(?string $caPath): self
+    {
+        $this->caPath = $caPath;
+
+        return $this;
     }
 }
